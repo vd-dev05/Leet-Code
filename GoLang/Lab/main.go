a21ffabbe53c174a46a35c0e823fd56aa875f6ef
duonghoccodefullstack <dbui99620.com>
2025-03-24 00:37:19 +0700

    GoLang

--- GoLang/Lab/main.go
+++ GoLang/Lab/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+// Hello word
+// func main() {
+// 	fmt.Println("Hello, Golang!")
+// }
 
-func main() {
-	fmt.Println("Hello, Golang!")
-}
+// Sum
+// func sum(a int, b int) int {
+// 	return a + b
+// }
+
+// func main() {
+// 	a := sum(1, 2)
+// 	println(a)
+// }
+
+// check number is prime
+
+// func isEven(n int) bool {
+// 	return n%2 == 0
+// }
+
+// func main() {
+// 	println(isEven(2)) // true
+// 	println(isEven(3)) // false
+// }
