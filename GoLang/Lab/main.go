36ee8a1eb8a7a323682add726f67d95aa6ed7883
duonghoccodefullstack <dbui99620.com>
2025-03-24 00:43:19 +0700

    GoLang

--- GoLang/Lab/main.go
+++ GoLang/Lab/main.go
@@ -25,3 +25,32 @@ package main
 // 	println(isEven(2)) // true
 // 	println(isEven(3)) // false
 // }
+
+//  sum Array
+// func sumArray(arr []int) int {
+// 	total := 0
+// 	for _, num := range arr {
+// 		total += num
+// 	}
+// 	return total
+// }
+
+// func main() {
+// 	arr := []int{1, 2, 3, 4, 5}//15
+// 	println(sumArray(arr))
+// }
+
+//  check number is prime
+func isPrime(n int) bool {
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	println(isPrime(7))
+	println(isPrime(8))
+}
